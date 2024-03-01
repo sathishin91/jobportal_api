ebca7d2ef2a4704202ab65ab0634290590e37984
theVaibhavRJoshi <joshi.monty1000@gmail.com>
2024-03-01 18:15:18 +0530

    new api added for employee

--- application/controllers/api/SeekkMobile.php
+++ application/controllers/api/SeekkMobile.php
@@ -1751,4 +1751,81 @@ class SeekkMobile extends CI_Controller
 
         self::setOutPut();
     }
+
+
+    /*
+     * get user profie details by user id
+     */
+    public function getUserProfileByUserId()
+    {
+        $isAuth = $this->ApiCommonModel->decodeToken();
+        if ($isAuth == 1) {
+            $json_data = json_decode(file_get_contents("php://input"));
+
+            $api_key           = $json_data->api_key;
+            $user_id           = $json_data->user_id;
+
+            if ($json_data) {
+                // $api_key = $this->input->post('api_key');
+                $api_key = $json_data->api_key;
+
+                if ($this->ApiCommonModel->checkApiKey($api_key)) {
+                    $reqData = $json_data;
+                    $reqData = (array) $reqData;
+
+                    if (!empty($api_key)) {
+
+                        $this->form_validation->set_data($reqData);
+                        $this->form_validation->set_rules('user_id', 'User Id', 'required|trim');
+
+                        if ($this->form_validation->run() == TRUE) {
+
+                            $result = $this->CommonModel->select_rec('user', '*', array('id' => $user_id, 'role_id' => 4))->result_array();
+
+
+                            if ($result) {
+                                // $result = $this->UserModel->update('skill_info', $userData, array('user_id' => $user_id));
+
+                                $this->responseData['code']         = 200;
+                                $this->responseData['status']       = 'success';
+                                $this->responseData['message']      = 'Profile fetched successfully.';
+                                $this->responseData['data']         = $result;
+                            } else {
+                                $this->responseData['code']    = 404;
+                                $this->responseData['message'] = 'Not fetched successfully!';
+                                $this->responseData['status']  = 'failed';
+                            }
+                        } else {
+                            $msg = $this->ApiCommonModel->validationErrorMsg();
+                            $this->responseData['code']    = 400;
+                            $this->responseData['status']  = 'failed';
+                            $this->responseData['message'] = $msg;
+                        }
+                    } else {
+                        $this->responseData['code']    = 404;
+                        $this->responseData['status']  = 'failed';
+                        $this->responseData['message'] = 'Required param missing: user_id!';
+                    }
+                } else {
+                    $this->responseData['code']    = 400;
+                    $this->responseData['status']  = 'failed';
+                    $this->responseData['message'] = 'Invalid api key!';
+                }
+            } else {
+                $this->responseData['code']    = 400;
+                $this->responseData['status']  = 'failed';
+                $this->responseData['message'] = 'Invalid request!';
+            }
+        } elseif ($isAuth == 0) {
+            $this->responseData['code']    = 400;
+            $this->responseData['status']  = 'failed';
+            $this->responseData['message'] = 'Token is invalid or expired!';
+        } else {
+            $this->responseData['code']    = 400;
+            $this->responseData['status']  = 'failed';
+            $this->responseData['message'] = 'Bearer Token required!';
+        }
+
+        self::setOutPut();
+    }
 }
