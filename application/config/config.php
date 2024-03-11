5552260ab4b517ad8afdc8fa778832c9352826a3
sathish kumar <sathishin91@gmail.com>
2024-03-11 09:39:57 +0530

    Server changes

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/codeigniter/seekk_api/seekk_api/api/';
+$config['base_url'] = 'https://sktexus.com/seekk_api/api/';
 
 /*
 |--------------------------------------------------------------------------
